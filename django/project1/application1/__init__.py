f77e16a56d734b407c195f3fd93372b2227fa097
fin33rus <fin33rus@gmail.com>
2019-10-03 17:07:41 +0300

    Доделал админку

---
+++ django/project1/application1/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'application1.apps.Application1Config'
--- django/project1/application1/apps.py
+++ django/project1/application1/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class Application1Config(AppConfig):
     name = 'application1'
+    verbose_name = 'Приложение 1'
