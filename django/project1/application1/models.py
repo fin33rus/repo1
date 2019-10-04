7645982244622ff9a71c90cf63f867fe6fd25465
fin33rus <fin33rus@gmail.com>
2019-10-04 16:13:56 +0300

    Работаю со статическими файлами и шаблонами

--- django/project1/application1/models.py
+++ django/project1/application1/models.py
@@ -20,6 +20,7 @@ class WahaGame(models.Model):
     name = models.CharField(max_length=30, verbose_name='Название игры')
     short_description = models.CharField(max_length=30, verbose_name='Краткое описание')
     price = models.IntegerField(default=0, verbose_name='Цена')
+    photo = models.ImageField('Фото', upload_to='application1/photos', default='', blank=True)
 
     def __str__(self):
         return self.name
--- django/project1/project1/urls.py
+++ django/project1/project1/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [ 
     path('admin/', admin.site.urls),
     path('application1/', include('application1.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
